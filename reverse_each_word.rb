07662165cbef269afbb07a0381c84b2228655057
Nathan Rathe <nprathe10@gmail.com>
2019-05-31 18:06:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string = string.split
   new_string = ""
   string.collect do |r|
-    r.reverse = new_string
+    new string = r.reverse
 
   end
   new_string.join(" ")
